d27ff9a4ee0563bef61a3c4c33d16e4f6ac2b9c5
NataGCM <156731754+NataGCM@users.noreply.github.com>
2024-10-05 22:40:46 -0500

    Add files via upload

---
+++ conectlocal/app/src/main/java/com/example/conectlocal/ServiciosActivity.kt
@@ -0,0 +1,13 @@
+package com.example.conectlocal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ServiciosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_servicios)
+    }
+}
+
